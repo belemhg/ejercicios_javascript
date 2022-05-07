03d2a00426970ff84eabc1f15b27d6b84e2ea48f
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-07 14:49:16 -0500

    if y console.log

--- ejercicio03/ejercicio03.js
+++ ejercicio03/ejercicio03.js
@@ -1,22 +1,44 @@
 console.log('Hola desde el ejemplo 03');
 // Ejemplo de switch
 
-let diaSemana = 3
-switch(diaSemana){
+let diaSemana = 7      // cambiar valores para poder hacer ejercicios
+switch (diaSemana) {
     case 1:
-    console.log('///////');
-    console.log('lunes');
-    break;
+        console.log('///////');
+        console.log('lunes');
+        break;
     case 2:
-    console.log('******');
-    console.log('Martes'); 
-    break;
+        console.log('******');
+        console.log('Martes');
+        break;
     case 3:
-    console.log('******');   
-    console.log('miercoles'); 
-    break;
+        console.log('******');
+        console.log('miercoles');
+        break;
+    case 4:
+        console.log('///////');
+        console.log('Jueves');
+        break;
+    case 5:
+        console.log('///////');
+        console.log('viernes');
+        break;
+
+    case 6:// sabado
+    case 7://domingo
+
+ if(diaSemana === 6){
+     console.log ("Es sabado");
+ }   else {
+     console.log('Es domingo');
+ }
+
+
+
+        console.log('Es sabado o domingo dia de descanso');
+        break;
     default:
-    console.log ('Dia de la semana invalido');
-    break;
+        console.log('Dia de la semana invalido');
+        break;
 
 }
