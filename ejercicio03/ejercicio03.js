7f3d895240fc95674f442e1f8a8e5cfd4334dbe5
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-07 13:53:33 -0500

    ejemplo de switch

--- ejercicio03/ejercicio03.js
+++ ejercicio03/ejercicio03.js
@@ -1,1 +1,22 @@
 console.log('Hola desde el ejemplo 03');
+// Ejemplo de switch
+
+let diaSemana = 3
+switch(diaSemana){
+    case 1:
+    console.log('///////');
+    console.log('lunes');
+    break;
+    case 2:
+    console.log('******');
+    console.log('Martes'); 
+    break;
+    case 3:
+    console.log('******');   
+    console.log('miercoles'); 
+    break;
+    default:
+    console.log ('Dia de la semana invalido');
+    break;
+
+}
