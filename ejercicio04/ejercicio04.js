f5439a7751e07b5a510907b4776980649f5f863a
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-07 15:23:37 -0500

    ejemplos de ciclos for

--- ejercicio04/ejercicio04.js
+++ ejercicio04/ejercicio04.js
@@ -1,1 +1,16 @@
 console.log('Ejemplos de ciclo For');
+
+// declaro una variable llamada ciclos, de la cual va iniciar con el valor de inicio 1
+// cuando los ciclos sean mayor < o igual =  se va ir incrementando hasta el  fin 
+
+let inicio = 1;
+let fin = 10;
+
+for ( let ciclos  = inicio; ciclos <= fin; ciclos++) {
+      console.log('numero es igual a '  + ciclos)
+
+}
+
+
+
+
