b065e3678e0eb144cd8c30329c3d38f426f4c602
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-07 15:27:23 -0500

    variable stop

--- ejercicio04/ejercicio04.js
+++ ejercicio04/ejercicio04.js
@@ -3,12 +3,14 @@ console.log('Ejemplos de ciclo For');
 // declaro una variable llamada ciclos, de la cual va iniciar con el valor de inicio 1
 // cuando los ciclos sean mayor < o igual =  se va ir incrementando hasta el  fin 
 
-let inicio = 1;
+let inicio = 1;  // recorrido del 1 al 10
 let fin = 10;
+let stop =5     
+// para detener el recorrido se agrega una variable llamada Stop, 
+// para que no termine en 10
 
 for ( let ciclos  = inicio; ciclos <= fin; ciclos++) {
-      console.log('numero es igual a '  + ciclos)
-
+      console.log('numero es igual a '  + ciclos);
 }
 
 
