e3300dcb316d286bf8cc0e001bac9a540e4a6d60
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-08 15:11:08 -0500

    arreglos segunda parte

---
+++ ejercicio06/ejercicio06.js
@@ -0,0 +1,10 @@
+console.log('arreglos segunda parte');
+
+let arreglo=[50,80,72,60,96];
+console.log ('Antes---------');
+console.log(arreglo);
+
+arreglo.splice(2, 1, 99);
+console.log('Despues----------');
+console.log(arreglo);
+
