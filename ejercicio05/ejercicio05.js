b6fa1d2aefe1cc1846aed23d937f09623b9394e5
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-07 21:15:43 -0500

    ciclos y arreglos

--- ejercicio05/ejercicio05.js
+++ ejercicio05/ejercicio05.js
@@ -1,4 +1,20 @@
-console.log ('--------');
+console.log('--------');
 console.log('Manipulacion de arreglos');
 console.log('--------');
+let arreglo = [10, 20, 30];
+console.log(arreglo);
+arreglo.push(40);
+console.log('***************');
+console.log(arreglo);
+
+for(let ciclo = 0; ciclo <= 3; ciclo++){
+    console.log('El valor es -> '+arreglo[ciclo]);
+
+}
+
+
+
+
+/// metodo o funciones
+/// sinonimo de acciones
 
