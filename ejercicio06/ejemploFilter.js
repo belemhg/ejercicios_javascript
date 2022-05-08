1709c9e34edb496967f7249f56bc175048576395
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-08 17:15:31 -0500

    Filter

--- ejercicio06/ejemploFilter.js
+++ ejercicio06/ejemploFilter.js
@@ -2,13 +2,19 @@ console.log('Ejemplo Filter');
 let arreglo=[50,80,72,60,96];
 console.log(arreglo)
 
+// operador OR ||
+
 let arregloDos = arreglo.filter(
-    function(value){
-       value > 50
+    function(value,index,){
+        // And && Or
+      return (value > 50 || index === 2);
     }
 )
 
+
+
+
 console.log("------->");
-console.log("Arreglo Dos");
+console.log("ArregloDos");
 
 
