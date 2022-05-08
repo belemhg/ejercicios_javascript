e2d36e84bfaeb5bfd8da0e9fd669f59596f2460d
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-08 13:30:45 -0500

    arreglos

--- ejercicio05/ejercicio05.js
+++ ejercicio05/ejercicio05.js
@@ -2,14 +2,12 @@ console.log('--------');
 console.log('Manipulacion de arreglos');
 console.log('--------');
 let arreglo = [10, 20, 30];
-console.log(arreglo);
-arreglo.push(40);
+arreglo.push([50]);
 console.log('***************');
 console.log(arreglo);
 
 for(let ciclo = 0; ciclo <= 3; ciclo++){
     console.log('El valor es -> '+arreglo[ciclo]);
-
 }
 
 
