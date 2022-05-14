af6821c4cf600a605143fd15fc1c4d4aca6c2f3e
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-14 14:35:27 -0500

    principios de ECMA

---
+++ ejercicio08/ejercicio08.js
@@ -0,0 +1,16 @@
+console.log('Templates');
+// EMAC6 LET es parte de CONST
+
+let FirtName = 'Belem Hernandez';
+let age = 40;
+
+// Esta parte de las plantillas y la interpolacion usando el signo de pesos $ 
+// y comillas diagonales para las variables.
+
+console.log ( `Mi nombre es ${FirtName} y mi edad es ${age}`);
+
+// el templates se usa para hacer inserciones de valores pero utilizando la forma de arriba//
+
+
+
+
