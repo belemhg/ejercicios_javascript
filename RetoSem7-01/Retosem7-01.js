464e81d77ca41ec88438ccf7cfa9ebd5ed729e47
belemhg <pehernandez.dwfc6@ucamp.utel.edu.mx>
2022-05-16 17:29:36 -0500

    cambiar nombre arreglo

---
+++ RetoSem7-01/Retosem7-01.js
@@ -0,0 +1,26 @@
+// Crear una funcion que me cambie el valor del nombre 
+// de cualquier disco en mi arreglo.
+// cambiarNombre (posicionArreglo, nuevoNombreDisco);
+// crear una funcion que me cambie el valor del nombre 
+// de cualquier disco que yo quiera en mi arrreglo.
+
+let tiendaDiscos = ['discoRock,discobalada,discoIndie'];
+let posicionArreglo = parseInt(prompt ('cambiar el nombre el disco'));
+let nuevoNombreDisco = prompt ('nuevo nombre de disco');
+
+posicionArreglo = Number (posicionArreglo);
+
+
+const cambiarNombre = (positionDisc, nuevoNombreDisco) => {
+    tiendaDiscos.forEach(
+        (disco, index) =>{ 
+            if (positionDisc === index){
+                disco.nameDisc = nuevoNombreDisco;
+                console.log (disco);
+            }
+        }
+    )
+}
+
+cambiarNombre(posicionArreglo, nuevoNombreDisco);
+console.log(tiendaDiscos);
